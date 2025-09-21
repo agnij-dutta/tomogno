6cd90bb30791c48516df96cdb624ff116da7654e
agnij-dutta <duttaagnij01@gmail.com>
2025-09-21 15:41:42 +0530

    dobby is free

---
+++ fhERC/contracts/tokens/SimpleERC20.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title SimpleERC20
+ * @notice A simple ERC20 token for testing purposes
+ */
+contract SimpleERC20 is ERC20 {
+    uint8 private _decimals;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals_
+    ) ERC20(name, symbol) {
+        _decimals = decimals_;
+        // No initial mint - tokens will be minted by tests as needed
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
